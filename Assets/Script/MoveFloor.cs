f9c41e7f0edf636261d8b1ef51da48141ba22817
yamato1551 <47923511+yamato1551@users.noreply.github.com>
2019-03-06 02:41:36 +0900

    1.9

--- Assets/Script/MoveFloor.cs
+++ Assets/Script/MoveFloor.cs
@@ -6,7 +6,7 @@ public class MoveFloor : MonoBehaviour
 {
     public float distance;
     public float speed;
-    public bool moveflagx,moveflagy,moveflagz;
+    public bool moveflagx, moveflagy, moveflagz;
     private Vector3 initialPosition;
     void Start()
     {
@@ -18,16 +18,26 @@ public class MoveFloor : MonoBehaviour
     {
         if (moveflagx)
         {
-            transform.position = new Vector3(Mathf.Sin(Time.time*speed) * distance + initialPosition.x, initialPosition.y, initialPosition.z);
+            transform.position = new Vector3(Mathf.Sin(Time.time * speed) * distance + initialPosition.x, initialPosition.y, initialPosition.z);
         }
         if (moveflagy)
         {
-            transform.position = new Vector3(initialPosition.x, Mathf.Sin(Time.time*speed) * distance + initialPosition.y, initialPosition.z);
+            transform.position = new Vector3(initialPosition.x, Mathf.Sin(Time.time * speed) * distance + initialPosition.y, initialPosition.z);
         }
         if (moveflagz)
         {
-            transform.position = new Vector3(initialPosition.x, initialPosition.y,Mathf.Sin(Time.time*speed) * distance +  initialPosition.z);
+            transform.position = new Vector3(initialPosition.x, initialPosition.y, Mathf.Sin(Time.time * speed) * distance + initialPosition.z);
         }
 
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        //触れているオブジェクトを子オブジェクトにする
+        collision.gameObject.transform.SetParent(this.transform);
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        //子オブジェクトをnullにする
+        collision.gameObject.transform.SetParent(null);
     }
 }
