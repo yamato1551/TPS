afb5713896b4fe3c4a8b9f946f9f2623ac62cefe
yamato1551 <47923511+yamato1551@users.noreply.github.com>
2019-04-04 00:04:28 +0900

    2.8

--- Assets/Script/OverorClear.cs
+++ Assets/Script/OverorClear.cs
@@ -4,17 +4,23 @@ using UnityEngine;
 using UnityEngine.UI;
 public class OverorClear : MonoBehaviour
 {
+    public AudioClip Over,Clear;
+    private AudioSource audiosource;
     // Start is called before the first frame update
     void Start()
     {
+        audiosource = gameObject.GetComponent<AudioSource>();
         if (StageManager.Result)
         {
+            audiosource.clip = Clear;
             this.GetComponent<Text>().text = "GameClear";
         }
         else
         {
+            audiosource.clip = Over;
             this.GetComponent<Text>().text = "GameOver";
         }
+        audiosource.Play();
     }
 
     // Update is called once per frame
